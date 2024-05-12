db32031c7a3708451bf751db35f92e4780a4ce73
Salah Abbas <0utWrest@protonmail.com>
2024-05-12 01:33:46 -0700

    init

---
+++ exploring-attacks/jpeg/DiffJPEG.py
@@ -0,0 +1,37 @@
+# original source: https://github.com/mlomnitz/DiffJPEG
+
+# Pytorch
+import torch
+import torch.nn as nn
+# Local
+from jpeg.modules import compress_jpeg, decompress_jpeg
+from jpeg.utils import diff_round, quality_to_factor
+
+
+class DiffJPEG(nn.Module):
+    def __init__(self, height, width, device, differentiable=True, quality=80):
+        ''' Initialize the DiffJPEG layer
+        Inputs:
+            height(int): Original image hieght
+            width(int): Original image width
+            differentiable(bool): If true uses custom differentiable
+                rounding function, if false uses standrard torch.round
+            quality(float): Quality factor for jpeg compression scheme. 
+        '''
+        super(DiffJPEG, self).__init__()
+        if differentiable:
+            rounding = diff_round
+        else:
+            rounding = torch.round
+        factor = quality_to_factor(quality)
+        self.compress = compress_jpeg(rounding=rounding, factor=factor, device=device)
+        self.decompress = decompress_jpeg(height, width, rounding=rounding,
+                                          factor=factor, device=device)
+
+    def forward(self, x):
+        '''
+
+        '''
+        y, cb, cr = self.compress(x)
+        recovered = self.decompress(y, cb, cr)
+        return recovered
---
+++ exploring-attacks/jpeg/modules/__init__.py
@@ -0,0 +1,5 @@
+# original source: https://github.com/mlomnitz/DiffJPEG
+
+# python3
+from .compression import compress_jpeg
+from .decompression import decompress_jpeg
